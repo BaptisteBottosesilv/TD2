24d3eee51f218b6f48348117ca43fef945283eca
BaptisteBottosesilv <157008519+BaptisteBottosesilv@users.noreply.github.com>
2024-04-05 21:28:02 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from bs4 import BeautifulSoup
+from datetime import datetime
+pip install Flask-Caching
+from flask import Flask
+from flask_caching import Cache
 
 app = Flask(__name__)
+cache = Cache(app, config={'CACHE_TYPE': 'SimpleCache'})
 
-@app.route('/')
+
+@app.route('/https://deepmind.google/discover/blog/tacticai-ai-assistant-for-football-tactics/')
+@cache.cached(timeout=50)
+
 def index():
-    return 'Hello, World!'
+
+    return ("Hello world")
 
 if __name__ == '__main__':
     app.run()
