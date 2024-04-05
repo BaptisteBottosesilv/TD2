df231afe1ad29c6e95738b5831d08d80eb7ab01d
BaptisteBottosesilv <157008519+BaptisteBottosesilv@users.noreply.github.com>
2024-04-05 21:35:10 +0200

    Update server.py

--- server.py
+++ server.py
@@ -9,10 +9,23 @@ app = Flask(__name__)
 cache = Cache(app, config={'CACHE_TYPE': 'SimpleCache'})
 
 
-@app.route('/https://deepmind.google/discover/blog/tacticai-ai-assistant-for-football-tactics/')
+@app.route('/get_latest_article_info')
 @cache.cached(timeout=50)
 
 def index():
+     url = 'https://deepmind.google/discover/blog/'
+     response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        last_article = soup.select_one('article_selector')
+        author = last_article.select_one('author_selector').get_text(strip=True)
+        date_published = last_article.select_one('date_selector').get_text(strip=True)
+        return jsonify({
+            'author': author,
+            'date_published': date_published
+        })
+    else:
+        return jsonify({'error': 'Failed to retrieve data'}), 500
 
     return ("Hello world")
 
