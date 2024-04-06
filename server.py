4c17a0c496ca4532632eeb5f4b6c3cfaf1a08e0e
BaptisteBottosesilv <157008519+BaptisteBottosesilv@users.noreply.github.com>
2024-04-06 14:07:31 +0200

    Update server.py

--- server.py
+++ server.py
@@ -13,20 +13,6 @@ cache = Cache(app, config={'CACHE_TYPE': 'SimpleCache'})
 @cache.cached(timeout=50)
 
 def index():
-     url = 'https://deepmind.google/discover/blog/'
-     response = requests.get(url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        last_article = soup.select_one('article_selector')
-        author = last_article.select_one('author_selector').get_text(strip=True)
-        date_published = last_article.select_one('date_selector').get_text(strip=True)
-        return jsonify({
-            'author': author,
-            'date_published': date_published
-        })
-    else:
-        return jsonify({'error': 'Failed to retrieve data'}), 500
-
     return ("Hello world")
 
 if __name__ == '__main__':
